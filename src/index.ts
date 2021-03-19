c0ba4d57c10bea70cb0d2a0c58f7b47728c9bd32
evaou <speeder528@gmail.com>
2021-03-19 08:19:55 +0800

    add html report and static method

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,13 @@ import { CsvFileReader } from './CsvFileReader';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
 import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
+import { HtmlReport } from './reportTargets/HtmlReport';
 
 const csvFileReader = new CsvFileReader('football.csv');
 
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(
-  new WinsAnalysis('Man United'),
-  new ConsoleReport()
-);
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
 summary.buildAndrPrintReport(matchReader.matches);
